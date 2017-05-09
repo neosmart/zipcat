d82e59e1c42b3657d6138a2cc81ed8a9a066ad46
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-05-09 16:34:15 -0500

    Include/exclude filter support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate getopts;
+extern crate glob;
 extern crate zip;
 
 use getopts::Options;
+use glob::{MatchOptions, Pattern};
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
@@ -12,10 +14,28 @@ fn main() {
     let program = args[0].clone();
 
     let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("s",
+                 "silent",
+                 "suppress file names from being sent to stderr");
+    opts.optmulti("x",
+                  "exclude",
+                  "exclude file(s) matching pattern (can use more than once)",
+                  "PATTERN");
+    opts.optmulti("i",
+                  "include",
+                  "include only file(s) matching pattern (can use more than once)",
+                  "PATTERN");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(f) => panic!(f.to_string()),
+    };
+
+    let parsed = ParsedOptions {
+        suppress_file_names: matches.opt_present("s"),
+        include_globs: matches.opt_strs("i").iter().map(|str| Pattern::new(str).unwrap()).collect(),
+        exclude_globs: matches.opt_strs("x").iter().map(|str| Pattern::new(str).unwrap()).collect(),
     };
 
     if matches.free.is_empty() {
@@ -25,7 +45,7 @@ fn main() {
 
     let sources = matches.free;
     for source in sources {
-        cat(&source).unwrap_or_else(|e| {
+        cat(&source, &parsed).unwrap_or_else(|e| {
             writeln!(&mut std::io::stderr(),
                      "Failed to cat contents of zip file {}: {:?}",
                      source,
@@ -36,17 +56,47 @@ fn main() {
 }
 
 fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} ZIPFILE [options]", program);
-    print!("{}", opts.usage(&brief));
+    println!("Usage: {} ZIPFILE [options]", program);
+    print!("Pipes content of compressed file(s) within a zip archive to stdout");
+    print!("{}", opts.usage(""));
 }
 
-fn cat(source: &str) -> Result<()> {
-    let mut file = File::open(source)?;
+struct ParsedOptions {
+    suppress_file_names: bool,
+    include_globs: Vec<Pattern>,
+    exclude_globs: Vec<Pattern>,
+}
+
+fn include_file(path: &str, includes: &Vec<Pattern>, excludes: &Vec<Pattern>) -> bool {
+    let options = MatchOptions {
+        case_sensitive: false,
+        require_literal_separator: false,
+        require_literal_leading_dot: false,
+    };
+
+    if includes.len() == 0 {
+        return !excludes.iter().any(|p| p.matches_with(path, &options));
+    }
+
+    return includes.iter().any(|p| p.matches_with(path, &options)) &&
+           !excludes.iter().any(|p| p.matches_with(path, &options));
+}
+
+fn cat(source: &str, options: &ParsedOptions) -> Result<()> {
+    let file = File::open(source)?;
     let mut zip = zip::ZipArchive::new(file)?;
 
     for i in 0..zip.len() {
         let mut z_file = zip.by_index(i)?;
-        writeln!(&mut std::io::stderr(), "{}: ", z_file.name())?;
+        if !include_file(z_file.name(),
+                         &options.include_globs,
+                         &options.exclude_globs) {
+            continue;
+        }
+
+        if !options.suppress_file_names {
+            writeln!(&mut std::io::stderr(), "{}: ", z_file.name())?;
+        }
 
         let mut stdout = std::io::stdout();
         let mut buffer = [0; 1024];
